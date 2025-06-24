3eab94f1b038d54254493360db2c17b8a8d36eed
ganesh-neurealm <ganesh.singhpapola@neurealm.com>
2025-06-24 17:12:12 +0530

    persist data

--- app/store/todo.ts
+++ app/store/todo.ts
@@ -1,7 +1,15 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import type { TodoStore } from '@/app/types/todo';
 
-export const useTodoStore = create<TodoStore>((set) => ({
-  todos: [],
-  setTodos: (todos) => set({ todos })
-}));
+export const useTodoStore = create<TodoStore>()(
+  persist(
+    (set) => ({
+      todos: [],
+      setTodos: (todos) => set({ todos }),
+    }),
+    {
+      name: 'todo-store', 
+    }
+  )
+);
